d5b7fd96340b8bb3c8a476d90690489cf63375eb
tubi55 <tubi55@nate.com>
2024-09-26 15:22:04 +0900

    지도에 마커 추가 (위치가 아직 정밀하지 않음)

--- js/kakao.js
+++ js/kakao.js
@@ -3,9 +3,21 @@ const mapContainer = document.querySelector("#map");
 
 //지도에 적용할 옵션값을 객체로 묶어서 할당
 const mapOption = {
-	center: new kakao.maps.LatLng(33.450701, 126.570667), //출력할 지도의 위도, 경수
-	level: 3 //지도의 확대 정도
+	center: new kakao.maps.LatLng(37.512674, 127.060603), //출력할 지도의 위도, 경도
+	level: 1 //지도의 확대 정도
 };
 
 //new 연산자로 카카오 지도 인스턴스를 생성 (지도를 출력한 DOM, 지도옵션객체)
 const map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 지도위치에 관한 인스턴스 생성
+const markerPosition = new kakao.maps.LatLng(37.512674, 127.060603);
+
+// 마커에 대한 인스턴스 생성 (인수로 위에서 생성된 지도위치 인스턴스 필요)
+const marker = new kakao.maps.Marker({
+	position: markerPosition
+});
+
+// 마커인스턴스에 setMap함수를 호출해서 인수로 지도 인스턴스 집어넣음
+//지도에 마커가 출력
+marker.setMap(map);
