a354dd8feb35929cba91c453f2d8cc4015c9d2a5
tubi55 <tubi55@nate.com>
2024-09-26 16:12:37 +0900

    확대, 축소 컨트롤러 추가

--- js/kakao.js
+++ js/kakao.js
@@ -9,8 +9,13 @@ const map = new kakao.maps.Map(mapContainer, mapOption);
 const marker = new kakao.maps.Marker({ position: mapOption.center });
 marker.setMap(map);
 
-//맵위에 올릴 컨트롤 인스턴스 생성
+//맵위에 올릴 타입 컨트롤 인스턴스 생성
 const mapTypeControl = new kakao.maps.MapTypeControl();
 
 //타입컨트롤러의 위치값 지정하는 함수
 map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+//맵 위에 올릴 줌 컨트롤 인스턴스 생성
+const zoomControl = new kakao.maps.ZoomControl();
+//줌 컨트롤러의 위치값 지정하는 함수
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
